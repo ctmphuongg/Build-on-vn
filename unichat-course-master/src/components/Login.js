87150c0454d41c093aaa76ab73888c497fdca3e8
ctmphuongg <phuong.caothiminh.contact@gmail.com>
2021-08-17 13:04:01 +0700

    sth

---
+++ unichat-course-master/src/components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { GoogleOutlined, FacebookOutlined } from 'ant-design/icons';
+
+const Login = () => {
+        return ( < div id = 'page' > < /div> );
+        }
+
+        export default Login;
